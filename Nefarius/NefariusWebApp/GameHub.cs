8548765eb79dce5455a44e83ed91915088ea588d
Igor <charsequence+github@gmail.com>
2018-03-07 12:50:25 +0300

    Game data updates after each player turned

--- Nefarius/NefariusWebApp/GameHub.cs
+++ Nefarius/NefariusWebApp/GameHub.cs
@@ -58,7 +58,7 @@ namespace NefariusWebApp
             }
             var player = GetPlayer(Context.ConnectionId);
             _gameTicker.Game.Turning(player, action);
-            Clients.Client(Context.ConnectionId).InvokeAsync("PlayerData", player);
+            _gameTicker.BroadcastGame();
         }
 
         public void Spy(decimal pTo, decimal pFrom = 0)
