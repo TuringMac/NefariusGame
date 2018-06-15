58aa3106e481b079f99f075bc4fa84cd66623e29
Igor Baranov <charsequence@gmail.com>
2018-06-15 16:19:55 +0300

    Angular template

---
+++ Nefarius/NefariusAngular/ClientApp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  private hubConnection: HubConnection;
+  nick = '';
+  message = '';
+  messages: string[] = [];
+
+  ngOnInit() {
+    this.nick = window.prompt('Your name:', 'John');
+
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl("/game")
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+  }
+}
